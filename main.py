8501c69bdefff2c9f4ebf1cf919cb73a72f47d34
exile21 <addinmalik@gmail.com>
2023-07-19 00:47:27 +0700

    update

--- main.py
+++ main.py
@@ -2,44 +2,30 @@
 from mq2 import MQ as mq2
 from mq4 import MQ as mq4
 from mq9 import MQ as mq9
-from irsensor import detect_flame
 import time
 from sklearn.neural_network import RBFRegressor
 import numpy as np
-import RPi.GPIO as GPIO
-import Adafruit_DHT
+from Adafruit_DHT import DHT11, read_retry
 
 # DHT11
-sensor = Adafruit_DHT.DHT11
+sensor = DHT11
 dht_pin = 18
 
-# IR SENSOR
-GPIO.setmode(GPIO.BCM)
-flame_pin = 5
-GPIO.setup(flame_pin, GPIO.IN)
-
-# MQ SENSORS
 mq2 = mq2(analogPin=0)
 mq4 = mq4(analogPin=2)
 mq9 = mq9(analogPin=1)
-
 try:
     while True:
-        # humidity, temperature = Adafruit_DHT.read_retry(sensor, dht_pin)
-        gas_percentage_mq9 = mq9.MQPercentage()
-        gas_percentage_mq4 = mq4.MQPercentage()
         gas_percentage_mq2 = mq2.MQPercentage()
+        gas_percentage_mq4 = mq4.MQPercentage()
+        gas_percentage_mq9 = mq9.MQPercentage()
         
-        # if GPIO.input(flame_pin) == GPIO.LOW:
-        #     print('Fire detected')
-        # else:
-        #     print('No fire detected')
+        humidity, temperature = read_retry(sensor, dht_pin)
+        if humidity is not None and temperature is not None:
+            print(f'Temperature: {temperature}°C, Humidity: {humidity}%')
+        else:
+            print('Failed to retrieve data from humidity sensor')
         
-        # if humidity is not None and temperature is not None:
-        #     print(f'Temperature: {temperature}°C, Humidity: {humidity}%')
-        # else:
-        #     print('Failed to retrieve data from humidity sensor')
-            
         print("MQ-2 Gas Percentage (LPG):", gas_percentage_mq2["GAS_LPG"])
         print("MQ-2 Gas Percentage (CO):", gas_percentage_mq2["CO"])
         print("MQ-2 Gas Percentage (Smoke):", gas_percentage_mq2["SMOKE"])
@@ -47,9 +33,8 @@ try:
         print("MQ-4 Gas Percentage (CNG):", gas_percentage_mq4["CNG"])
         print("MQ-9 Gas Percentage (CO):", gas_percentage_mq9["CO"])
         print("MQ-9 Gas Percentage (Flammable):", gas_percentage_mq9["FLAMMABLE"])
-        
         time.sleep(1)
 except KeyboardInterrupt:
-    print('Program terminated')
+    print("Program stopped by user")
 finally:
-    GPIO.cleanup()
+    print("Program terminated")
