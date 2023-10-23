cec1dc95c27524beb853c6f2df1e3381542579e2
Addin Malik <addinmalik@gmail.com>
2023-10-23 19:43:27 +0700

    add address

--- lcd.py
+++ lcd.py
@@ -5,7 +5,8 @@ lcd_columns = 16
 lcd_rows = 2
 
 # Initialize LCD
-lcd = LCD.Adafruit_CharLCDPlate()
+lcd_address = 0x27 # Replace with your LCD I2C address
+lcd = LCD.Adafruit_CharLCDPlate(address=lcd_address)
 
 # Print "Hello, World!" on LCD
 lcd.message('Hello,\nWorld!')
