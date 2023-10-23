4d6ebd4f2869478157e920bdc6d6408100fe855b
Addin Malik <addinmalik@gmail.com>
2023-10-23 19:36:36 +0700

    add address

--- main_non_rbf.py
+++ main_non_rbf.py
@@ -27,7 +27,8 @@ saved_model = load_model("rbf_classification_model.h5", custom_objects={"RBFLaye
 # Initialize the LCD screen with I2C backpack module
 lcd_columns = 16
 lcd_rows = 2
-lcd = LCD.Adafruit_CharLCDPlate()
+lcd_address = 0x27 # Replace with your LCD I2C address
+lcd = LCD.Adafruit_CharLCDPlate(address=lcd_address)
 
 try:
     while True:
